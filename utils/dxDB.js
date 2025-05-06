512baa1cb035fda265b61e9204b41ef87c5313eb
lixingyangok <176840078@qq.com>
2025-05-06 12:41:37 +0800

    优化 dexie.js 加载方式

--- utils/dxDB.js
+++ utils/dxDB.js
@@ -2,27 +2,37 @@
  * @Author: Merlin
  * @Date: 2024-01-07 21:07:28
  * @LastEditors: Merlin
- * @LastEditTime: 2025-02-09 15:42:28
+ * @LastEditTime: 2025-05-06 09:23:43
  * @Description: 
  */
 
 export const useDexie = (()=>{
-    if (!import.meta.client) return ()=>{};
-    let dxDB = null;
+    if (!import.meta.client) return () => null;
+    const oResolvers = {};
     return async ()=>{
-        if (dxDB || !import.meta.client) return dxDB;
-        const {Dexie} = await import('https://cdn.jsdelivr.net/npm/dexie@3.2.7/+esm');
-        dxDB = new Dexie("dxDB");
+        if (!import.meta.client) return null;
+        // console.log('Try to use dexie.js 📦');
+        if (oResolvers.promise) {
+            return oResolvers.promise; 
+        }
+        // console.log('Started to: import(dexie.js); 📦'); // 只会打印1次 
+        Object.assign(
+            oResolvers,
+            Promise.withResolvers(),
+        );
+        const {Dexie} = await import('https://cdn.jsdelivr.net/npm/dexie@4.0.11/+esm');
+        const dxDB = new Dexie("dxDB");
         // ↓ 每次修改建库参数需要加大版本号
-        dxDB.version(18).stores({
+        dxDB.version(19).stores({
             // 表名：索引列,（非索引列不需要添加
             text: '++id, &mediaId, createdAt, updatedAt, type', // type=pdf|txt
-            sqlite: "++id, createdAt, updatedAt, type", // type=main|cache
+            sqlite: "++id, createdAt, updatedAt, type, [type+updatedAt]", // type=main|cache
             directory: '++id, createdAt',
         });
         if (typeof window === 'object'){
             window.dxDB = dxDB; // 便于调试
         }
-        return dxDB;
+        oResolvers.resolve(dxDB);
+        return oResolvers.promise;
     };
 })();
